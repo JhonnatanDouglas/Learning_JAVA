8517516766f28e3d6964b7ed0fcb349520116d95
Jhonnatan Douglas <jhonnatanaraujodev@gmail.com>
2024-07-01 17:42:49 -0300

    Classes ArrayList e String

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,59 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        var meuCarro = new Carro();
 
-        // Public
-        System.out.println(meuCarro.marca);
-        meuCarro.marca = "Toyota";
-        System.out.println(meuCarro.marca);
+        // Classe de Inteiros 
+        var numeros = new ArrayList<Integer>();
 
-        // Protected
-        System.out.println(meuCarro.ano);
+        System.out.println(numeros.size()); // 0
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        System.out.println(numeros.size()); // 3
+        System.out.println(numeros.get(0)); // 1
 
-        // Private
-        System.out.println(meuCarro.getModelo());
+        System.out.println(numeros.remove(2)); // 3
+        System.out.println(numeros.size()); // 2
+
+        numeros.clear();
+        System.out.println(numeros.size()); // 0
+
+        System.out.println(numeros.isEmpty()); // true
+
+
+        System.out.println(numeros.size()); // 0
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(10);
+        System.out.println(numeros.size()); // 3
+
+        var value = 10;
+        System.out.println(numeros.contains(value)); // true
+
+        numeros.set(0, 999); // seta para os indeces que EXISTEM, os que nao existem ele nao cria, da erro
+        System.out.println(numeros.get(0)); // 999
+
+
+        // Classe de Strings
+        var nome = "Leonhart";
+
+        nome.length(); // Verificar o tamanho
+        nome.toLowerCase(); // Transformar para caixa baixa
+        nome.trim(); // remove todo espaço branco dentro da string
+        nome.contains("a"); // verifica se "a" contem dentro da string, seria true
+
+        var frutas = "maça, banana, morango";
+        frutas.split(","); // removera todas as virgulas
+
+        // Classe de double/int
+        var personagem = "Annie";
+        var idade = 18;
+        var altura = 1.67;
+
+        var resumo = String.format("Nome: %s, Idade: %d, Altura: %2f", personagem, idade, altura);
+        System.out.println(resumo);
+
     }
 }
